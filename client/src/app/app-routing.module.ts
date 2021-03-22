cb44999822db1c4a5dd137adf4f0defcf4e78395
RameshwarRai <59338706+RameshwarRai@users.noreply.github.com>
2021-03-22 11:09:26 +0530

    End of Section 7

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { ServerErrorComponent } from './errors/server-error/server-error.component';
+import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 import { HomeComponent } from './home/home.component';
 import { ListsComponent } from './members/lists/lists.component';
 import { MemberDeatilComponent } from './members/member-deatil/member-deatil.component';
@@ -17,12 +20,13 @@ const routes: Routes = [
     {path:'members',component:MemberListComponent,canActivate :[AuthGuard]},
     {path:'members/:id',component:MemberDeatilComponent},
     {path:'lists',component:ListsComponent	},
-    {path:'messages',component:MessagesComponent},
-    {path:'**',component:HomeComponent,pathMatch:"full"}
+    {path:'messages',component:MessagesComponent}
   ]
-}
-
-
+},
+{path:"errors",component:TestErrorsComponent},
+{path:"not-found",component:NotFoundComponent},
+{path:"server-error",component:ServerErrorComponent},
+  {path:'**',component:NotFoundComponent,pathMatch:"full"}
 ];
 
 @NgModule({
